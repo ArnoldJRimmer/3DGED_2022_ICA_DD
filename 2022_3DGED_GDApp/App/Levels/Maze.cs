0f1113f15e44297a35085a957131d3df3bb08d95
ArnoldJRimmer <d00240561@student.dkit.ie>
2022-12-28 22:25:28 +0000

    :sparkle: added a maze class

---
+++ 2022_3DGED_GDApp/App/Levels/Maze.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+namespace GD.App
+{
+    public class Maze
+    {
+        #region Fields
+        public const int mazeWidth = 20;
+        public const int mazeHeight = 20;
+        GraphicsDevice myDevice;
+        VertexBuffer floorBuffer;
+        Color[] floorColors = new Color[2]
+        {
+            Color.White,
+            Color.Black,
+        };
+        #endregion
+
+        #region Constructor
+        public Maze(GraphicsDevice _myDevice)
+        {
+            this.myDevice = _myDevice;
+            BuildFloorBuffer();
+        }
+        #endregion
+
+        #region The Floor
+        private void BuildFloorBuffer()
+        {
+            List<VertexPositionColor> vertexList = new List<VertexPositionColor>();
+            int counter = 0;
+
+            for (int x = 0; x < mazeWidth; x++)
+            {
+                counter++;
+                for (int z = 0; z < mazeHeight; z++)
+                {
+                    foreach (VertexPositionColor vertex in FloorTile(x, z, floorColors[counter % 2]))
+                    {
+                        vertexList.Add(vertex);
+                    }
+                }
+            }
+
+            floorBuffer = new VertexBuffer(myDevice, VertexPositionColor.VertexDeclaration, vertexList.Count, BufferUsage.WriteOnly);
+            floorBuffer.SetData<VertexPositionColor>(vertexList.ToArray());
+
+        }
+
+        private List<VertexPositionColor> FloorTile(int x_OffSet, int z_Offset, Color tileColor)
+        {
+            List<VertexPositionColor> verticesList = new List<VertexPositionColor>();
+
+            //Tidy up all tile positions using AppData.cs
+            verticesList.Add(new VertexPositionColor(new Vector3(0 + x_OffSet, 0, 0 + z_Offset), tileColor));
+            verticesList.Add(new VertexPositionColor(new Vector3(1 + x_OffSet, 0, 0 + z_Offset), tileColor));
+            verticesList.Add(new VertexPositionColor(new Vector3(0 + x_OffSet, 0, 1 + z_Offset), tileColor));
+            verticesList.Add(new VertexPositionColor(new Vector3(1 + x_OffSet, 0, 0 + z_Offset), tileColor));
+            verticesList.Add(new VertexPositionColor(new Vector3(1 + x_OffSet, 0, 1 + z_Offset), tileColor));
+            verticesList.Add(new VertexPositionColor(new Vector3(0 + x_OffSet, 0, 1 + z_Offset), tileColor));
+
+            return verticesList;
+
+        }
+        #endregion
+    }
+}
