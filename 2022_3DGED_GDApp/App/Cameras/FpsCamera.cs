6b60466c4501e3140eb4ba331d7371353e90e81c
ArnoldJRimmer <d00240561@student.dkit.ie>
2022-12-28 22:26:17 +0000

    :sparkle: added a fps camera class

--- 2022_3DGED_GDApp/App/Cameras/FpsCamera.cs
+++ 2022_3DGED_GDApp/App/Cameras/FpsCamera.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,94 @@ namespace GD.App
 {
    public class FpsCamera
    {
+        #region Fields
+        private Vector3 position = Vector3.Zero;
+        //I need a point the camera is going to be looking at, and a way of determining the direction as well
+        private Vector3 lookAt;
+        private Vector3 basePointOfReference = new Vector3(0, 0, 1);
+        private Matrix storedViewMatrix;
+        private bool needViewResync = true;
+        private float rotation;
+        #endregion
 
-   }
+        #region Properties
+        public Matrix Projection
+        {
+            get;
+            private set;
+        }
+
+        public Vector3 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+                UpdateLookAt();
+            }
+        }
+
+        public float Rotation
+        {
+            get
+            {
+                return rotation;
+            }
+            set
+            {
+                rotation = value;
+                UpdateLookAt();
+            }
+        }
+
+        /// <summary>
+        /// I have a storing mechanism in place so that everytime the view matrix is calculated,#
+        /// i can store it and get at easyily if needed.
+        /// The view matirx takes in the camera position, the point we just looked at and a vector telling us the direction of UP for the camera
+        /// </summary>
+        public Matrix View
+        {
+            get
+            {
+                if (needViewResync)
+                {
+                    storedViewMatrix = Matrix.CreateLookAt(Position, lookAt, Vector3.Up);
+                }
+
+                return storedViewMatrix;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+        public FpsCamera(Vector3 position, float rotation, float aspectRatio, float nearClip, float farClip)
+        {
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, nearClip, farClip);
+            //This method allows you to specify where the camera is in a single call
+            MoveTo(position, rotation);
+        }
+        #endregion
+
+        #region HelperMethods
+        private void UpdateLookAt()
+        {
+            Matrix rotationMatrix = Matrix.CreateRotationY(rotation);
+            Vector3 lookAtOffSet = Vector3.Transform(basePointOfReference, rotationMatrix);
+            lookAt = position + lookAtOffSet;
+            needViewResync = true;
+        }
+
+        public void MoveTo(Vector3 _position, float _rotation)
+        {
+            this.position = _position;
+            this.rotation = _rotation;
+            UpdateLookAt();
+
+        }
+        #endregion
+    }
 }
